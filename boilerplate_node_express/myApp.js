ed1fb9c4de3cf2c26cbda2bdca6796b8bf669966
Bisrat-Araya <62281539+Bisrat-Araya@users.noreply.github.com>
2022-07-01 10:06:06 +0200

    serving json with .env variables

--- boilerplate_node_express/myApp.js
+++ boilerplate_node_express/myApp.js
@@ -1,6 +1,67 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
+console.log("Hello World")
+// console.log(process.env.MESSAGE_STYLE)
+
+// app.get('/', function(req, res) {
+//   res.send('Hello Express')
+// })
+
+let absolutePath = __dirname + "/views/index.html"
+
+app.get('/', function(req, res) {
+  res.sendFile(absolutePath)
+})
+
+let assetAbsolutePath = __dirname + "/public"
+
+let middleware = express.static(assetAbsolutePath)
+app.use('/public', middleware)
+
+app.get('/json', function(req, res) {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({"message": "Hello json".toUpperCase()})
+  } else {
+    res.json({"message": "Hello json"})
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
 
 
 
