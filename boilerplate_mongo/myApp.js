9fc469835db8e06a7c3bae335780cbc9e7192b59
Bisrat-Araya <62281539+Bisrat-Araya@users.noreply.github.com>
2022-07-03 04:51:35 +0200

    added a few more dependencies and connectd w mongoose

--- boilerplate_mongo/myApp.js
+++ boilerplate_mongo/myApp.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
+const mongoose = require('mongoose')
 
+const mongo_uri = process.env.MONGO_URI
+mongoose.connect(mongo_uri)//, { useNewUrlParser: true, useUnifiedTopology: true})
 
 let Person;
 
